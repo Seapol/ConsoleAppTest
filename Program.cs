75c821aa50cc5bfc063fe6de60c8b811ffe901c0
Seapol Shen <seapol.shen@infineon.com>
2021-10-07 12:12:57 +0800

    1007

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web;
 
 namespace ConsoleAppTest
 {
@@ -10,6 +11,27 @@ namespace ConsoleAppTest
     {
         static void Main(string[] args)
         {
+            List<TestAction> actions = new List<TestAction>();
+
+            for (int i = 0; i < 2; i++)
+            {
+                TestAction action = new TestAction();
+               
+                actions.Add(action);
+            }
+
+            foreach (var item in actions)
+            {
+                //item.DoWork();
+
+                item.Run();
+
+            }
+
+
+
+            Console.ReadKey();
+
         }
     }
 }
---
+++ TestAction.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Net;
+
+namespace ConsoleAppTest
+{
+    class TestAction
+    {
+
+        public TestAction()
+        {
+
+        }
+
+        public void DoWork()
+        {
+            int CursorLeft = 0;
+            int CursorTop = Thread.CurrentThread.ManagedThreadId + 1 ;
+
+            for (int i = 0; i < 1000; i++)
+            {
+                Console.SetCursorPosition(CursorLeft, CursorTop);
+                Console.WriteLine(string.Format("{0}: {1}", Thread.CurrentThread.ManagedThreadId, i));
+                Thread.Sleep(10);
+            }
+
+            WebClient client = new WebClient();
+            string result = client.DownloadString("https://www.baidu.com/");
+            Console.WriteLine(result);
+        }
+
+        public void Run()
+        {
+            Thread thread = new Thread(new ThreadStart(DoWork));
+
+            thread.Start();
+            thread.Join();
+        }
+    }
+}
